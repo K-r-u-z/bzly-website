9a7ee0cab3cd289a501585f48d957b38ef06b49b
Kruz <135771649+K-r-u-z@users.noreply.github.com>
2024-11-28 08:11:59 -0500

    Vercel making me lose it atp

--- next.config.js
+++ next.config.js
@@ -18,33 +18,8 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   output: 'standalone',
-  webpack: (config, { isServer }) => {
-    config.watchOptions = {
-      ...config.watchOptions,
-      ignored: ['**/.next/**', '**/node_modules/**', '**/.git/**']
-    };
-    config.resolve.fallback = { 
-      ...config.resolve.fallback,
-      punycode: false 
-    };
-    config.optimization = {
-      ...config.optimization,
-      minimize: true,
-      moduleIds: 'deterministic',
-      splitChunks: {
-        chunks: 'all',
-        cacheGroups: {
-          default: false,
-          vendors: false,
-          commons: {
-            name: 'commons',
-            chunks: 'all',
-            minChunks: 2,
-            reuseExistingChunk: true
-          }
-        }
-      }
-    };
+  webpack: (config) => {
+    config.resolve.fallback = { punycode: false };
     return config;
   }
 }
